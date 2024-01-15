9f6fea9e1c940c1f2a71bbd4ad446456cd97dd4c
65160366 <152172480+65160366@users.noreply.github.com>
2024-01-16 00:07:41 +0700

    Summary

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use Illuminate\Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
-
+use App\Http\Controllers\C_titles;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,19 +20,19 @@ Route::get('/', function () {
     return 'hi';
 });
 
-// Route::get('/my-route', function () {
-//     //echo "<h1> My Route Page</h1>";
-//     //return view('')
-//     //return viem('myroute');
-//     $data = ['val_a' => 'Hello World!'];
-//     $data['val_b'] = "Lalaval";
-//     return view('myfolder.mypage',$data);
-// });
+Route::get('/my-route', function () {
+    //echo "<h1> My Route Page</h1>";
+    //return view('')
+    //return viem('myroute');
+    $data = ['val_a' => 'Hello World!'];
+    $data['val_b'] = "Lalaval";
+    return view('myfolder.mypage',$data);
+});
 
-// Route::post('/my-route',function(Request $req){
-//     $data['myinput'] = $req -> input('myinput');
-//     return view ('myroute',$data);
-// });
+Route::post('/my-route',function(Request $req){
+    $data['myinput'] = $req -> input('myinput');
+    return view ('myroute',$data);
+});
 
 Route::get('/my-display', function () {
     $data['test'] = "test";
@@ -45,3 +45,15 @@ Route::post('/my-display',function(Request $req) {
 });
 
 Route::get('/admin', [AdminController::class, 'index']);
+
+Route::resource('titles', C_titles::class);
+
+Route::get('/my-controller' , [MyController::class, 'index']);
+
+Route::get('/my-controller2' , 'App\Http\Controllers\MyController@index');
+Route::namespace('App\Http\Controllers')->group(function(){
+    Route::get('/my-controller3','MyController@index');
+    Route::post('/my-controller3-post' , 'MyController@store');
+});
+
+Route::resource('/my-controller4' , MyController::class);
